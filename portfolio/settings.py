07f97290092afc8bb94f548428e670568153afd9
Anoop Ambooken <anoop.ambooken@kpn.com>
2018-10-06 13:50:36 +0200

    postgres migrate

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -71,10 +71,15 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

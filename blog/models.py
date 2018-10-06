439c85119d6649bdc8acf7009deb0a01b7525f27
Anoop Ambooken <anoop.ambooken@kpn.com>
2018-10-06 15:15:29 +0200

    home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.functions import datetime
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=200)
+    image = models.ImageField(upload_to='images/blog')
+    pub_date = models.DateTimeField()
